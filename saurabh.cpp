db29c544466dea0c3425648fa35e5a2301c6f7a2
Saurabh <prashantrangdale1234@gmail.com>
2023-08-20 22:01:53 +0530

    res func done

--- saurabh.cpp
+++ saurabh.cpp
@@ -7,6 +7,7 @@ class shopping{
     float dis;
     string pname;
 public:
+
     void menu();
     void administrator();
     void buyer();
@@ -18,6 +19,8 @@ public:
 
 };
 
+
+// edit will edit the products
 void shopping ::edit()
 {
     fstream data, data1;
@@ -73,6 +76,43 @@ void shopping ::edit()
         }
     }
 }
+
+// res will delete the product
+void shopping::res(){
+    fstream data, data1;
+    int pkey;
+    int token;
+    cout<<"\n\n\t Delete Product";
+    cout<<"\n\n\t Product code :";
+    cin>>pkey;
+    data.open("database.txt", ios::in);
+
+    if(!data){
+        cout<<"File do not exist";
+    }
+    else{
+        data1.open("database1.txt", ios::app | ios::out);
+        data>>pcode>>pname>>price>>dis;
+        while(!data.eof()){
+            if(pcode == pkey){
+                cout<<"\n\n\t Product deleted successfully";
+                token++;
+            }
+            else{
+                data1<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<"\n";
+            }
+        }
+        data.close();
+        data1.close();
+        remove("database.txt");
+        rename("database1.txt", "database.txt");
+
+        if(token == 0){
+            cout<<"\n\n Record cannot be Found";
+        }
+    }
+}
+
 
 
 
