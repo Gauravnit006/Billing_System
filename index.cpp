4388c516f77215344e4178767af8471958d2aa55
Saurabh <prashantrangdale1234@gmail.com>
2023-08-20 19:20:26 +0530

    s1

--- index.cpp
+++ index.cpp
@@ -71,6 +71,21 @@ void shopping ::add(){
 
 }
 
+void shopping ::edit()
+{
+    fstream data, data1;
+    int pkey;
+    int token = 0;
+    int c;
+    float p;
+    float d;
+    string n;
+
+    cout<<"\n\t\t\t Modify the Record";
+    cout<<"\n\t\t\t Product code :";
+    cin>>pkey;
+}
+
 int main(){
 
     cout<<"change this code";
