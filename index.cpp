04b6e6eba6382ed24398c653a27ec6e449ff0d2b
“Gaurav” <gauravnit006@gmail.com>
2023-08-20 18:22:23 +0530

    Shopping class

--- index.cpp
+++ index.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class shopping{
+    int pcode;
+    float price;
+    float dis;
+    string pname;
+public:
+    void menu();
+    void administrator();
+    void buyer();
+    void add();
+    void edit();
+    void res();
+    void list();
+    void receipt();
+
+};
+
 int main(){
 
 
